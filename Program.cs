e92edad4a6f316c12d7e834d65506731a3ef76a5
Vladimir <vova_spirid@mail.ru>
2023-02-18 22:25:13 +0400

    pull request

--- Program.cs
+++ Program.cs
@@ -30,7 +30,26 @@ void NumberArray(int[] a)
     Console.WriteLine($"Вы ввели {count} положительных чисел");
 }
 
+
+
+
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
 //y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+Console.Write("Введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+x = Math.Round(x, 2);
+y = Math.Round(y, 2);
+Console.WriteLine($"Точка пересечения прямых (x) и (y): ({x};{y})");
